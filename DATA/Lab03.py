34267a2a4b40d04a61f0f2de9e3020b07d56c930
Seven <sson@plu.edu>
2025-03-14 20:50:04 -0700

    Implemented rest of main and analyzeData

--- DATA/Lab03.py
+++ DATA/Lab03.py
@@ -2,6 +2,7 @@ import csv
 import os
 import random
 import argparse
+import pandas as pd
 
 
 def splitData(dataset):
@@ -15,12 +16,28 @@ def splitDataRandom(dataset):
     shuffled = dataset.copy()       # Copy the dataset
     random.shuffle(shuffled)        # Randomly shuffle the dataset (for more randomness)
 
-    # Portion and assign the data into 70%, 15%, and 15% respectively
+    # Portion and assign the data into 80% and 20% respectively
     split_index = int(len(shuffled) * 0.8)
     train_data = shuffled[:split_index]
     test_data = shuffled[split_index:]
 
     return train_data, test_data
+
+def analyzeData(dataset, header):
+
+    df = pd.DataFrame(dataset, columns=header)
+
+    for col in df.columns:
+        try:
+            df[col] = pd.to_numeric(df[col])
+        except ValueError:
+            continue
+    
+    print("Dataframe statistics:")
+    print("Mean:\n", df.mean(numeric_only=True))
+    print("Max:\n", df.max(numeric_only=True))
+    print("Min:\n", df.min(numeric_only=True))
+    print("Correlation:\n", df.corr(numeric_only=True))
 
 def main():
 
@@ -28,9 +45,21 @@ def main():
     parser.add_argument("--file", required=True, help="Path to the dataset")
     args = parser.parse_args()
 
-    filePath = os.path.abspath("Most Streamed Spotify Songs 2024.csv")
+    filePath = os.path.abspath(args.file)
 
     with open(filePath, newline='', encoding='ISO-8859-1') as csvfile:
         reader = csv.reader(csvfile)
         dataset = list(reader)
-pass
+
+    header = dataset[0]
+    dataset = dataset[1:]
+
+    train_data, test_data = splitDataRandom(dataset)
+
+    print(f"Train data: {len(train_data)}")
+    print(f"Test data: {len(test_data)}")
+
+    analyzeData(train_data, header)
+
+if __name__ == "__main__":
+    main()
